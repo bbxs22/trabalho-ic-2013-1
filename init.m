878491d3c2861c48c14213660ff4713760b9188f
Bruna <Xavier>
2013-05-04 14:35:16 -0300

    Ajuste das condições iniciais do problema
    - O ângulo phi inicial é um valor aleatório entre [-90, 90]
    - Os limites para x e y são, respectivamente, [0, 200] e [0, 100]
    - Criação da função local random (init.m) para geração de números reais
    aleatórios entre dois valores

--- init.m
+++ init.m
@@ -2,18 +2,18 @@ function init()
     %Inicializa o programa
     
     clear all; close all; clc;
-    initPlot();
-    robot = initRobot(10, 10, pi/3);
+    initPlot();   
+    robot = initRobot(10, 10, random(-pi/2, pi/2));
     plotRobot(robot);
 end
 
 function initPlot()
     %Inicializa o grafico
     %Eixo x de 0 a 200
-    %Eixo y de -50 a 50
+    %Eixo y de 0 a 100
     
-    xlim([0 50]);
-    ylim([-20 20]);
+    xlim([0 200]);
+    ylim([0 100]);
 end
 
 function [robot] = initRobot(x, y, angle)
@@ -21,7 +21,17 @@ function [robot] = initRobot(x, y, angle)
     %@param x: posicao inicial do robo no eixo x
     %@param y: posicao inicial do robo no eixo y
     %@param angle: angulo inicial de visao do robo (em radianos)
+    %@return o robo
     
     step = [12 12]; %passo dado pelo robo no movimento (x, y)
     robot = [[x, y]; [angle 0]; step];
+end
+
+function [r] = random(min, max)
+    %Geracao de numero real aleatorio entre @min e @max
+    %@param min: valor minimo
+    %@param max: valor maximo
+    %@return valor aleatorio no intervalo [@min, @max]
+    
+    r = min + (max-(min)) .* rand();
 end
