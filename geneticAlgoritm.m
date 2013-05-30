58282205eb50dcb79a491b7f9b69e4ed5fb978c6
Bruna <Xavier>
2013-05-30 18:16:15 -0300

    Implementação do algoritmo genético

---
+++ geneticAlgoritm.m
@@ -0,0 +1,24 @@
+function [x, fval] = geneticAlgoritm()
+    clear all; close all; clc;
+    options = gaoptimset('EliteCount', 2, 'FitnessLimit', 0.3, 'Generations', 30, 'PopulationSize', 20, 'SelectionFcn', @selectionroulette);
+    [x, fval] = ga(@fitness, 37, [], [], [], [], [], [], [], options);
+    x = setVariables(x);
+end
+
+function y = fitness(x)
+    fis = readfis('robot');
+    
+    x = setVariables(x);
+    for i = 1 : 37
+        fis.rule(i).consequent = floor(x(i));
+    end
+
+    y = initSimulation(fis);
+    %disp(x)
+    %sprintf('Fitness: %.4f', y)
+end
+
+function y = setVariables(x)
+    y = mod(round(x * 1000), 7) + 1;
+end
+
