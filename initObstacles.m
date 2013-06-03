e2d9712b64d0e241a49bd4e94c7f61f946d231dd
Bruna <Xavier>
2013-06-03 04:20:36 -0300

    Alterando cenários de teste
    - Ao inves de usar somente um cenário de obstáculo (obstaculos em forma
    de V), são utilizados 3 cenários definidos em initObstacles.m
    * initFixedObstacles();
    * initFixedObstacles2();
    * initFixedObstacles3();
    - A média de colisões/passos é dado pelo valor médio destes três
    cenários
    - O robô varia o ângulo de -180º a 180º (de 45º em 45º)

--- initObstacles.m
+++ initObstacles.m
@@ -2,11 +2,32 @@ function [obstacles] = initObstacles()
     %Inicializa obstaculos
     %@return matriz de obstaculos (1 x total)
     
-    %obstacles = initFixedObstacles();
-    obstacles = initRandomObstacles(5);
+    obstacles = [initFixedObstacles(); initFixedObstacles2(); initFixedObstacles3()];
+    %obstacles = initRandomObstacles(5);
 end
 
 function [obstacles] = initFixedObstacles()
+    %Inicializa 5 obstaculos em posicoes predefinidas
+    %@return matriz de obstaculos (1 x total)
+    
+    obstacles(1).x = 60;
+    obstacles(1).y = 40;
+    obstacles(1).radius = 3;
+    obstacles(2).x = 100;
+    obstacles(2).y = 30;
+    obstacles(2).radius = 3;
+    obstacles(3).x = 80;
+    obstacles(3).y = 80;
+    obstacles(3).radius = 3;
+    obstacles(4).x = 120;
+    obstacles(4).y = 60;
+    obstacles(4).radius = 3;
+    obstacles(5).x = 160;
+    obstacles(5).y = 20;
+    obstacles(5).radius = 3;
+end
+
+function [obstacles] = initFixedObstacles2()
     %Inicializa 5 obstaculos em posicoes predefinidas
     %@return matriz de obstaculos (1 x total)
     
@@ -24,6 +45,27 @@ function [obstacles] = initFixedObstacles()
     obstacles(4).radius = 3;
     obstacles(5).x = 140;
     obstacles(5).y = 90;
+    obstacles(5).radius = 3;
+end
+
+function [obstacles] = initFixedObstacles3()
+    %Inicializa 5 obstaculos em posicoes predefinidas
+    %@return matriz de obstaculos (1 x total)
+    
+    obstacles(1).x = 60;
+    obstacles(1).y = 60;
+    obstacles(1).radius = 3;
+    obstacles(2).x = 80;
+    obstacles(2).y = 30;
+    obstacles(2).radius = 3;
+    obstacles(3).x = 80;
+    obstacles(3).y = 80;
+    obstacles(3).radius = 3;
+    obstacles(4).x = 120;
+    obstacles(4).y = 40;
+    obstacles(4).radius = 3;
+    obstacles(5).x = 160;
+    obstacles(5).y = 60;
     obstacles(5).radius = 3;
 end
 
--- initSimulation.m
+++ initSimulation.m
@@ -7,27 +7,38 @@ function [meanS, meanC] = collisionsFixedObstacles(fis)
     %initPlot();
     %plotObstacles(obstacles);
     
-    totalC = 0;
-    collisions = 0;
-    totalS = 0;
-    steps = 0;
-    for x = 10 : 10 : 50
-        for y = 10 : 10 : 90
-            for angle = -pi/2 : pi/3 : pi/2
-                robot = initRobot(x, y, angle, 1);
-                %plota o robo no inicio da simulacao
-                %plotRobot(robot);
-                stats = start(robot, obstacles, fis);
-                collisions = collisions + stats.collision;
-                totalC = totalC + 1;
-                if ~stats.collision
-                    steps = steps + stats.steps;
-                    totalS = totalS + 1;
+    meanS = ones(1, 3);
+    meanC = ones(1, 3);
+    
+    for i = 1 : 3
+        totalC = 0;
+        collisions = 0;
+        totalS = 0;
+        steps = 0;
+    
+        for x = 10 : 10 : 50
+            for y = 10 : 10 : 90
+                for angle = -pi : pi/4 : pi
+                    robot = initRobot(x, y, angle, 1);
+                    %plota o robo no inicio da simulacao
+                    %plotRobot(robot);
+                    stats = start(robot, obstacles(i, :), fis);
+                    collisions = collisions + stats.collision;
+                    totalC = totalC + 1;
+                    if ~stats.collision
+                        steps = steps + stats.steps;
+                        totalS = totalS + 1;
+                    end
                 end
             end
         end
+        meanS(1, i) = steps / totalS;
+        meanC(1, i) = collisions / totalC;
     end
     
-    meanS = steps / totalS;
-    meanC = collisions / totalC;
+    meanS = sum(meanS) / 3;
+    meanC = sum(meanC) / 3;
+    
+    %meanS = steps / totalS;
+    %meanC = collisions / totalC;
 end
